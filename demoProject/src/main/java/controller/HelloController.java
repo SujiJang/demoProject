544309896f94c751b8706992eaf63c7b16e71c26
USER <USER@AL01132044>
2018-03-22 14:28:34 +0900

    first demo upload

---
+++ demoProject/src/main/java/controller/HelloController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping(value = "/index.do", method = RequestMethod.GET)
+	public ModelAndView index() {
+		ModelAndView mav = new ModelAndView();
+	
+		mav.setViewName("hello");
+		return mav;
+	}
+}
